4088200bc4cda2d428a5da3a2d587b53c6c6fca4
GavinJudge <gavinjudge@gmail.com>
2022-12-09 13:59:56 +0000

    added simple python file

---
+++ simplePython.py
@@ -0,0 +1,10 @@
+#This is a simple python program to demonstrate how python works
+
+#Define a function
+def my_function(x):
+  return x * 5
+
+#Call the function
+print(my_function(2))
+
+#output should be 10
